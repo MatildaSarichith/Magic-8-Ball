04029dd9831c5da660a0d096e858edd31b129983
Matilda Sarichith <matisari676@ed.amdsb.ca>
2022-03-11 13:58:58 -0500

    Add project files.

---
+++ Magic 8 Ball/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Magic_8_Ball
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.eightBallButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label1.Location = new System.Drawing.Point(328, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(177, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Think of a question";
+            // 
+            // eightBallButton
+            // 
+            this.eightBallButton.FlatAppearance.BorderSize = 0;
+            this.eightBallButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.eightBallButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.eightBallButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.eightBallButton.Image = global::Magic_8_Ball.Properties.Resources.clickBall;
+            this.eightBallButton.Location = new System.Drawing.Point(163, 62);
+            this.eightBallButton.Name = "eightBallButton";
+            this.eightBallButton.Size = new System.Drawing.Size(528, 460);
+            this.eightBallButton.TabIndex = 1;
+            this.eightBallButton.UseVisualStyleBackColor = true;
+            this.eightBallButton.Click += new System.EventHandler(this.eightBallButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.outputLabel.Location = new System.Drawing.Point(311, 536);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(218, 25);
+            this.outputLabel.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(852, 613);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.eightBallButton);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button eightBallButton;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ Magic 8 Ball/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Magic_8_Ball
+{
+    public partial class Form1 : Form
+    {
+       Random randGen = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void eightBallButton_Click(object sender, EventArgs e)
+        {
+            int answer = randGen.Next(1, 7);
+
+            switch (answer)
+            {
+                case 1:
+                    outputLabel.Text = "Highly unlikely";
+                    break;
+                case 2:
+                    outputLabel.Text = "Don't count on it";
+                    break;
+                case 3:
+                    outputLabel.Text = "Most definitely";
+                    break;
+                case 4:
+                    outputLabel.Text = "Possibly";
+                    break;
+                case 5:
+                    outputLabel.Text = "Probably not";
+                    break;
+                case 6:
+                    outputLabel.Text = "The chances are high";
+                    break;
+                    default:
+                    break;
+            }
+        }
+    }
+}
